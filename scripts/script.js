40061889fc1a8ef4e308a21af128c7eaf3ced711
Christian Garlen <chadgarval@outlook.com>
2026-07-25 20:26:37 -0400

    Added more styles to my containers for each task, added dialog box to enter information, no functionality yet

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,16 @@ const status = ["Start", "In progress", "Completed", "Overdue"];
 
 const containerStructure = ["task-container", "taskTitle", "taskCategory", "taskDeadline", "progressBox", "task-progress-ball", "taskStatus"];
 
+const taskList = new Array();
+
+// I create my object constructor
+function task(taskTitle, category, deadline, status){
+    this.taskTitle = taskTitle;
+    this.category = category;
+    this.deadline = deadline;
+    this.status = status;
+}
+
 let tasks = new Array();
 
 function createTaskContainer(taskTitle, category, deadline){
@@ -14,39 +24,50 @@ function createTaskContainer(taskTitle, category, deadline){
     // the same css configuration
     // I'll use the method classList and add because I want to add some bootstrap from
     // the beginning
-    container.classList.add(containerStructure[0], "d-flex", "justify-content-center", "p-2"); 
+    container.classList.add(containerStructure[0], "d-flex", "flex-column", "justify-content-center", "p-2", "rounded-3", "col-md-4", "col-12"); 
 
     // The title of each task
     const title = document.createElement("h2");
-    header.classList.add(containerStructure[1], "text-center", "fw-bold", "text-white");
-    header.textContent = taskTitle;
+    title.classList.add(containerStructure[1], "text-center", "fw-bold", "text-white", "mb-0", "pt-2", "pb-2");
+    title.textContent = taskTitle;
     
     // Category for each task
-    const category = document.createElement("p");
-    header.classList.add(containerStructure[2]);
-    header.textContent = category;
+    const _category = document.createElement("p");
+    _category.classList.add(containerStructure[2], "m-0", "p-2");
+    _category.textContent = category;
 
     // Deadline for each task
-    const deadline = document.createElement("p");
-    header.classList.add(containerStructure[3]);
-    header.textContent = deadline;
+    const _deadline = document.createElement("p");
+    _deadline.classList.add(containerStructure[3], "m-0", "p-2");
+    _deadline.textContent = deadline;
 
     // A container for my status box
     const statusBox = document.createElement('div');
-    statusBox.classList.add(containerStructure[4], "d-flex", "gap-1");
+    statusBox.classList.add(containerStructure[4], "d-flex", "gap-1", "p-2", "align-items-center");
     
     // I'll create a container just to make a ball
     const statusBall = document.createElement('div');
-    statusBox.classList.add(containerStructure[5], "rounded-4");
+    statusBall.classList.add(containerStructure[5], "rounded-4");
 
     // This will create the status text
-    const status = document.createElement("p");
-    header.classList.add(containerStructure[6]);
-    header.textContent = status[0];
+    const _status = document.createElement("p");
+    _status.classList.add(containerStructure[6], "m-0");
+    _status.textContent = status[0];
 
     // This is a box, where I'll store 
     statusBox.appendChild(statusBall);
-    statusBox.appendChild(status);
+    statusBox.appendChild(_status);
 
-    document.main.appendChild(container);
+    container.appendChild(title);
+    container.appendChild(_category);
+    container.appendChild(_deadline);
+    container.appendChild(statusBox);
+
+    const main = document.getElementById("main-content");
+
+    main.appendChild(container);
 }
+
+// Demo
+createTaskContainer("Task 1", "Work", "Today");
+createTaskContainer("Task 2", "Class", "Yesterday");
